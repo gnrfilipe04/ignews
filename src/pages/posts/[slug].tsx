7871866d708ed81ea06058254037f1d2a23c3b11
gnrfilipe04 <gnr_filipe@hotmail.com>
2021-03-26 09:48:25 -0300

    :rocket: module finalized

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,5 +1,7 @@
 import { GetServerSideProps } from "next";
+import { Session } from "next-auth";
 import { getSession } from "next-auth/client";
+import { redirect } from "next/dist/next-server/server/api-utils";
 import Head from "next/head";
 import { RichText } from "prismic-dom";
 
@@ -42,6 +44,15 @@ export const getServerSideProps: GetServerSideProps = async ({
 }) => {
   const session = await getSession({ req });
   const { slug } = params;
+  
+  if(!session?.activeSubscription){
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
 
   const prismic = getPrismicClient(req);
 
